9d79fb55cb860ab6a5c01e34a2813b40ff795504
Jacques <jacquesvanluyck@gmail.com>
2023-12-28 16:01:22 +0100

    [finished][JAL]navigation route tracking

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent implements OnInit {
   pathTitle: string = "";
-
+  route: string = "";
+  routeSubscription: any;
   constructor(private router: Router) { }
 
   ngOnInit() {
-    if (this.router.url === '/') {
-      this.pathTitle = 'Login';
-    }
+    this.routeSubscription = this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        switch (event.url) {
+          case '/':
+            this.pathTitle = 'Login';
+            break;
+          case '/timer':
+            this.pathTitle = 'Timer';
+            break;
+          case '/overview':
+            this.pathTitle = 'Übersicht';
+            break;
+          default:
+            this.pathTitle = "";
+        }
+      }
+    })
   }
+
 }
