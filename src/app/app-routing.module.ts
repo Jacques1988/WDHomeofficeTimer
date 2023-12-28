e0562125cc8b8532d68a4b8ba2a7430887a4a454
Jacques <jacquesvanluyck@gmail.com>
2023-12-28 15:03:45 +0100

    [in Progress][JAL]add simple routing between components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { TimerComponent } from './timer/timer/timer.component';
+import { OverviewComponent } from './overview/overview/overview.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'timer', component: TimerComponent },
+  { path: 'overview', component: OverviewComponent }
 ];
 
 @NgModule({
