01d4a799154216d0ea55391bf97947ff9c350539
Jacques <jacquesvanluyck@gmail.com>
2023-12-28 10:01:07 +0100

    [ADD][JAL] add router logic to navigation component

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
   pathTitle: string = "";
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    if (this.router.url === '/') {
+      this.pathTitle = 'Login';
+    }
+  }
 }
