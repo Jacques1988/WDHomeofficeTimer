ba888f05622996fcc97a568df7e0454e0eafe38a
Jacques <jacquesvanluyck@gmail.com>
2024-01-07 21:43:28 +0100

    [Add][JAL] environment variables for fetch API and backend links, [in progress][JAL] add user model to backend

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    fetchApiUrlOverview: 'http://localhost:3000/overview',
+    setTimesDataApiUrl: 'http://localhost:3000/savetimes',
+    loginApiUrl: 'http://localhost:3000/login',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    fetchUrlOverview: 'http://localhost:3000/overview',
+    setTimesDataUrl: 'http://localhost:3000/savetimes',
+    loginUrl: 'http://localhost:3000/login',
+};
