b9311d52dcc63cb11477025de8c9f86a8d7ffb9c
Jacques <jacquesvanluyck@gmail.com>
2024-03-03 18:04:37 +0100

    löööft

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,8 @@
 export const environment = {
     production: false,
-    fetchUrlOverview: 'http://localhost/Backend/Worktimes.Class.php',
-    /* fetchUrlOverview: 'https://worktimetracker.bluewave-tech.de/timer/fetchUserTimes', */
-    setTimesDataUrl: 'http://localhost/Backend/Worktimes.Class.php',
-    /*  setTimesDataUrl: 'https://worktimetracker.bluewave-tech.de/timer/savetimes', */
-    loginUrl: 'http://localhost/Backend/Login.Class.php',
-    /* loginUrl: 'https://worktimetracker.bluewave-tech.de/login', */
-    signUpUrl: 'http://localhost/Backend/SignUp.Class.php',
-    /* signUpUrl: 'https://worktimetracker.bluewave-tech.de/signUp', */
+    fetchUrlOverview: 'https://tracker.bluewave-tech.de/backend/Worktimes.Class.php',
+    setTimesDataUrl: 'https://tracker.bluewave-tech.de/backend/Worktimes.Class.php',
+    loginUrl: 'https://tracker.bluewave-tech.de/backend/Login.Class.php',
+    signUpUrl: 'https://tracker.bluewave-tech.de/backend/SignUp.Class.php',
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    fetchUrlOverview: 'https://worktimetracker.bluewave-tech.de/timer/fetchUserTimes',
-    setTimesDataUrl: 'https://worktimetracker.bluewave-tech.de/timer/savetimes',
-    loginUrl: 'https://worktimetracker.bluewave-tech.de/login',
-    signUpUrl: 'https://worktimetracker.bluewave-tech.de/signUp',
+    fetchUrlOverview: 'https://tracker.bluewave-tech.de/backend/Worktimes.Class.php',
+    setTimesDataUrl: 'https://tracker.bluewave-tech.de/backend/Worktimes.Class.php',
+    loginUrl: 'https://tracker.bluewave-tech.de/backend/Login.Class.php',
+    signUpUrl: 'https://tracker.bluewave-tech.de/backend/SignUp.Class.php',
 };
